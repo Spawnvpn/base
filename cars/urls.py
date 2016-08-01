c9b307cc3426b4f208fccc6009aa10509b75d1db
Spawnvpn <spawnvpn@gmail.com>
2016-08-01 20:25:25 +0300

    Added pagination.

--- cars/urls.py
+++ cars/urls.py
@@ -9,6 +9,7 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', view=cars.views.CarListView.as_view(), name='car_list'),
+    url(r'^page/(?P<page>\d+)/$', view=cars.views.CarListView.as_view(), name='car_list'),
     url(r'^cars/(?P<pk>\d+)/$', view=cars.views.CarDetailView.as_view(), name='car_detail'),
     url(r'^cars/create/$', view=cars.views.CarCreateView.as_view(), name='car_create'),
     url(r'^cars/(?P<pk>\d+)/update/$', view=cars.views.CarUpdateView.as_view(), name='car_update'),
